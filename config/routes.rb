b60bb78c29794faafa67a01c3633b67a13f61575
Bilal <ranabilalrajput09@gmail.com>
2023-08-15 13:24:39 +0200

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'inventories#index'
-  resources :inventories, only: [:show, :index, :create ]
+  resources :inventories, only: %i[show index create]
 end
