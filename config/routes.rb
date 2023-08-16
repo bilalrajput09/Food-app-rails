572b53bf1969b3d2cfef9ff030b4ed9c0874ffb5
Ashnaali3255 <ashnaalipk@gmail.com>
2023-08-16 16:13:36 +0500

    Add recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'inventories#index'
+# Defines the root path route ("/")
+ root 'inventories#index'
   resources :inventories, only: %i[show index create] do
     resources :foods, only: %i[new create]
   end
+
+  resources :recipes do
+    # member do
+    #   patch :toggle_visibility
+    # end
+  end
+  get 'public_recipes', to: 'recipes#public_index', as: :public_recipes
 end
